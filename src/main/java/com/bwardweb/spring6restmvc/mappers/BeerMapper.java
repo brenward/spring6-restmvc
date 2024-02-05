b2ef8c41b048649a435a966baaa8dab24d6cc133
Brendan Ward <bren.ward@gmail.com>
2024-02-05 11:25:15 +0000

    Adding mappers from DTOs to Entities

---
+++ src/main/java/com/bwardweb/spring6restmvc/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package com.bwardweb.spring6restmvc.mappers;
+
+import com.bwardweb.spring6restmvc.entities.Beer;
+import com.bwardweb.spring6restmvc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+}
---
+++ src/main/java/com/bwardweb/spring6restmvc/mappers/CustomerMapper.java
@@ -0,0 +1,12 @@
+package com.bwardweb.spring6restmvc.mappers;
+
+import com.bwardweb.spring6restmvc.entities.Customer;
+import com.bwardweb.spring6restmvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+    Customer customerDtoToCustomer(CustomerDTO dto);
+
+    CustomerDTO customerToCustomerDto(Customer customer);
+}
