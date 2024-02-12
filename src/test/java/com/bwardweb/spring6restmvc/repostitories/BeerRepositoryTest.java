114a7aa24987d6154919a067838171ef3a2a3d7c
Brendan Ward <bren.ward@gmail.com>
2024-02-12 18:04:12 +0000

    Adding validation

--- src/test/java/com/bwardweb/spring6restmvc/repostitories/BeerRepositoryTest.java
+++ src/test/java/com/bwardweb/spring6restmvc/repostitories/BeerRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.bwardweb.spring6restmvc.repostitories;
 
 import com.bwardweb.spring6restmvc.entities.Beer;
+import com.bwardweb.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +20,35 @@ class BeerRepositoryTest {
 
     @Test
     void testSaveBeer(){
-        Beer savedBeer = beerRepository.save(Beer.builder().beerName("My Beer").build());
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyle.LAGER)
+                .upc("random")
+                .price(new BigDecimal("10.00"))
+                .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void testSaveBeerNameTooLong(){
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 012345678901234567890123456789012345678901234567890123456789")
+                    .beerStyle(BeerStyle.LAGER)
+                    .upc("random")
+                    .price(new BigDecimal("10.00"))
+                    .build());
+
+            beerRepository.flush();
+
+            assertThat(savedBeer).isNotNull();
+            assertThat(savedBeer.getId()).isNotNull();
+        });
+
     }
 }
