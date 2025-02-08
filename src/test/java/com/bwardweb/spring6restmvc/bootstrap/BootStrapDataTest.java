9393935c80721631937736ec599c7199fd6a70b0
brenward <bren.ward@gmail.com>
2025-02-08 18:13:31 +0000

    #fixing unit tests

--- src/test/java/com/bwardweb/spring6restmvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/com/bwardweb/spring6restmvc/bootstrap/BootStrapDataTest.java
@@ -3,15 +3,18 @@ package com.bwardweb.spring6restmvc.bootstrap;
 import com.bwardweb.spring6restmvc.repostitories.BeerRepository;
 import com.bwardweb.spring6restmvc.repostitories.CustomerRepository;
 import com.bwardweb.spring6restmvc.services.BeerCsvService;
+import com.bwardweb.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootStrapDataTest {
     @Autowired
     BeerRepository beerRepository;
@@ -32,7 +35,7 @@ class BootStrapDataTest {
     @Test
     void run() throws Exception {
         bootStrapData.run(null);
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
